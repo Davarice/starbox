d561a31911482fabd67bc94b0f2cf52654db7d14
Davarice <jsdreim@gmail.com>
2018-09-07 02:00:31 -0400

    Implementation of System and Belt class + many many misc additions

--- starbox/__init__.py
+++ starbox/__init__.py
@@ -4,5 +4,31 @@ import json
 import collections
 from .celestial import *
 
+#__all__ = ["genSol"]
+
 def starbox():
     pass
+
+
+def genSol():
+    solSys = System("Sol")
+    sol = Star("Sol", solSys)
+    p = Planet("Mercury", sol)
+    p = Planet("Venus", sol)
+    p = Planet("Earth", sol)
+    m = Planet("Luna", p)
+    p = Planet("Mars", sol)
+    m = Belt("Inner Belt", sol)
+    p = Planet("Jupiter", sol)
+    p.composition = "Gas"
+    p = Planet("Saturn", sol)
+    p.composition = "Gas"
+    m = Belt("Rings of Saturn", p)
+    p = Planet("Caelus", sol)
+    p.composition = "Gas"
+    p = Planet("Neptune", sol)
+    p.composition = "Gas"
+    p = Planet("Pluto", sol)
+    p.composition = "Ice"
+    m = Belt("Kuiper Belt", sol, composition={"ice":80.0,"rock":20.0})
+    return solSys
--- starbox/celestial.py
+++ starbox/celestial.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3.7
 import collections
-import decimal
 from scipy import constants
 
-__all__ = ["Planet","Star"]
+#__all__ = ["Planet","Star","System"]
 
 """
 /starbox/celestial.py
@@ -24,6 +23,24 @@ bodyRank: Hierarchy of mass of celestials
 
 """
 
+# Lookup tables for scale conversions
+mInUnit = {"mm":0.001,"m":1,"km":1000}
+gInUnit = {"mg":0.001,"g":1,"kg":1000}
+
+def convertMass(q1, u1, u2):
+    if u1 == u2:
+        return q1
+    else:
+        q0 = q1*gInUnit[u1]
+        q2 = q0/gInUnit[u2]
+        return q2
+
+def findLargestProportion(din,flavor=False):
+    dsort = [(k, din[k]) for k in sorted(din, key=din.get, reverse=True)]
+    return dsort[0][0]
+
+
+
 class Planet:
     """
     Planet: Most ubiquitous celestial body
@@ -35,24 +52,23 @@ class Planet:
     bodyType = "Planet"
 
     def __init__(self, name, parent=None, # Identity information
-                 composition="Rock", # Physical information
+                 composition="rock", # Physical information
                  ruler=None, space=None): # Social information
         self.name = name
         self.parent = parent
         self.composition = composition
-        self.ruler = ruler
         self.moons = [] # Natural bodies orbiting this body; Typically another planet
         self.satellites = [] # Synthetic structures orbiting this body; Typically a station
+
+        self.ruler = ruler
         self.sites = [] # Locations on the surface of the world, synthetic or geographical
         self.nations = [] # Entities controlling territory on this world (typically subservient to the ruler)
+
         self.bodyRank = 3
         try:
             self.parent.subAssign(self) # If the parent body has a specific method to integrate me, use it
         except AttributeError:
-            return None
-
-    def __str__(self):
-        return '"{}": A {} {} in the {} system'.format(self.name, self.composition.lower(), self.bodyType.lower(), self.system())
+            pass
 
     def system(self):
         try:
@@ -65,8 +81,23 @@ class Planet:
             self.moons.append(childNew)
             childNew.parent = self
             childNew.bodyRank = self.bodyRank + 1
+            if self.parent.bodyType == "System":
+                self.parent.subAssign(childNew)
         except AttributeError:
             return
+
+    def __str__(self):
+        oput = '"{}": A {} of {}'.format(self.name, self.bodyType.lower(), self.composition.lower())
+        system = self.system()
+        if system != None:
+            oput = oput + " in the {} system".format(system.name)
+        if self.ruler != self.parent.ruler:
+            oput = oput + ", under the control of {}".format(self.ruler)
+        if self.moons != []:
+            oput = oput + "\n -Has the following moons:"
+            for moon in self.moons:
+                oput = oput + "\n -- " + moon.__str__()
+        return oput
 
 
 class Star:
@@ -76,7 +107,7 @@ class Star:
     bodyType = "Star"
 
     def __init__(self, name, parent=None, # Identity information
-                 mass=1, stellarClass="D", subtype="Main Sequence", # Physical information
+                 mass=1.0, stellarClass="D", subtype="Main Sequence", # Physical information
                  ruler=None, space=None): # Social information
         self.name = name # STR: Common designation
         self.parent = parent # OBJ: Object around which this body orbits
@@ -87,32 +118,143 @@ class Star:
         self.planets = [] # Natural bodies orbiting this body; Typically planets and belts
         self.satellites = [] # Synthetic structures orbiting this body; Typically a station
         self.bodyRank = 2
+        try:
+            self.parent.subAssign(self) # If the parent body has a specific method to integrate me, use it
+        except AttributeError:
+            pass
 
     def system(self):
-        return self.system
+        try:
+            return self.parent.system()
+        except AttributeError:
+            return None
 
     def subAssign(self, childNew):
         try:
             self.planets.append(childNew)
             childNew.parent = self
             childNew.bodyRank = self.bodyRank + 1
+            if self.parent.bodyType == "System":
+                self.parent.subAssign(childNew)
         except AttributeError:
             return
 
-class MainSequence(Star):
-    pass
-
-class DwarfStar(Star):
-    pass
-
-class Pulsar(Star):
-    pass
-
-class NeutronStar(Star):
-    pass
 
 class BlackHole(Star):
-    pass
+    bodyType = "Black Hole"
+
+
+class System:
+    """
+    System: Standard designation for a grouping of bodies, typically headed by one or more stars
+    """
+    bodyType = "System"
+
+    def __init__(self, name, parent=None, # Identity information
+                 posPhi=0, posRho=0, mapCoords="", # Physical information
+                 ruler=None, space=None, rank=2): # Social information
+        self.name = name # STR: Common designation
+        self.parent = parent # OBJ: Object around which this body orbits
+        self.posPhi = posPhi
+        self.posRho = posRho
+        self.mapCoords = mapCoords
+        self.heads = []
+        self.bodies = []
+
+        self.ruler = ruler
+
+        self.bodyRank = rank
+        try:
+            self.parent.subAssign(self) # If the parent body has a specific method to integrate me, use it
+        except AttributeError:
+            pass
+
+    def system(self):
+        return self
+
+    def subAssign(self, childNew):
+        try:
+            if childNew.bodyRank > self.bodyRank:
+                self.bodies.append(childNew)
+            elif childNew.bodyRank == self.bodyRank:
+                self.heads.append(childNew)
+                """
+                for orbital in childNew.planets:
+                    self.bodies.append(orbital)
+                for orbital in childNew.moons:
+                    self.bodies.append(orbital)
+                for orbital in childNew.satellites:
+                    self.bodies.append(orbital)
+                for orbital in childNew.bodies:
+                    self.bodies.append(orbital)
+                """
+        except AttributeError:
+            pass
+
+    def __dict__(self):
+        return {"heads" : self.heads, "bodies" : self.bodies}
+
+
+class Belt:
+    """
+    Belt: A region of debris in orbit around a body, forming a ring
+    """
+
+    def __init__(self, name, parent=None, # Identity information
+                 posRho=0, composition={"rock":100.0}, # Physical information
+                 ruler=None, space=None, rank=3): # Social information
+        self.name = name # STR: Common designation
+        self.parent = parent # OBJ: Object around which this body orbits
+        self.posRho = posRho
+        self.composition = composition
+        self.orbitals = []
+
+        self.ruler = ruler
+
+        self.bodyRank = rank
+        try:
+            self.parent.subAssign(self) # If the parent body has a specific method to integrate me, use it
+        except AttributeError:
+            pass
+
+        if self.parent.bodyType == "Star":
+            self.bodyType = "Belt"
+        elif self.parent.bodyType == "Planet":
+            self.bodyType = "Ring"
+        else:
+            self.bodyType = "Cloud"
+
+    def system(self):
+        try:
+            return self.parent.system()
+        except AttributeError:
+            return None
+
+    def comp(self): # Calculate what the belt is "made of"
+        return findLargestProportion(self.composition,True)
+
+    def subAssign(self, childNew):
+        try:
+            self.orbitals.append(childNew)
+            childNew.parent = self
+            childNew.bodyRank = self.bodyRank + 1
+            if self.parent.bodyType == "System":
+                self.parent.subAssign(childNew)
+        except AttributeError:
+            pass
+
+    def __str__(self):
+        oput = '"{}": A {} of {}'.format(self.name, self.bodyType.lower(), self.comp().lower())
+        system = self.system()
+        if system != None:
+            oput = oput + " in the {} system".format(system.name)
+        if self.ruler != self.parent.ruler:
+            oput = oput + ", under the control of {}".format(self.ruler)
+        if self.orbitals != []:
+            oput = oput + "\n -Contains the following notable bodies:"
+            for moon in self.orbitals:
+                oput = oput + "\n -- " + moon.__str__()
+        return oput
 
 
 
